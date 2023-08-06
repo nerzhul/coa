c026b76ca2f80b6d10fbe54139d9a12206b77f9c
Loic Blot <loic.blot@unix-experience.fr>
2023-08-06 16:11:56 +0200

    add utoipa for openapi support

--- src/apiservice/main.rs
+++ src/apiservice/main.rs
@@ -1,17 +1,36 @@
 use axum::{
-    routing::{get, post},
+    routing,
     Router,
 };
+use utoipa::OpenApi;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+	#[derive(OpenApi)]
+    #[openapi(
+        paths(
+            issues::list_security_issues,
+            issues::list_configuration_issues,
+			billing::add_entries
+        ),
+        components(
+            schemas(billing::BillingEntry, billing::BillingResult)
+        ),
+        tags(
+            (name = "billing", description = "billing API")
+        )
+    )]
+	struct ApiDoc;
+
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/v1/billing", post(add_billing_entries));
+        .route("/", routing::get(root))
+		.route("/v1/issues/security", routing::get(issues::list_security_issues))
+		.route("/v1/issues/configuration", routing::get(issues::list_configuration_issues))
+        // `POST /v1/billing` register new billing entries
+        .route("/v1/billing", routing::post(billing::add_entries));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -28,6 +47,65 @@ async fn root() -> &'static str {
     "Coa API Service"
 }
 
-async fn add_billing_entries() -> &'static str {
-	"Billing added!"
+mod issues {
+	use axum::{
+		http::StatusCode,
+	};
+
+	#[utoipa::path(
+        get,
+        path = "/v1/issues/security",
+        responses(
+            (status = 200, description = "List all todos successfully", body = [BillingResult])
+        )
+    )]
+	pub(super) async fn list_security_issues() -> (StatusCode, &'static str) {
+		let r = "List Security Issues";
+		(StatusCode::OK, r)
+	}
+
+	#[utoipa::path(
+        get,
+        path = "/v1/issues/configuration",
+        responses(
+            (status = 200, description = "List all todos successfully", body = [BillingResult])
+        )
+    )]
+	pub(super) async fn list_configuration_issues() -> (StatusCode, &'static str) {
+		let r = "List Configuration Issues";
+		(StatusCode::OK, r)
+	}
+}
+
+mod billing {
+	use axum::{
+		http::StatusCode,
+    };
+	use utoipa::ToSchema;
+	use serde::{Deserialize, Serialize};
+
+	#[derive(Serialize, Deserialize, ToSchema, Clone)]
+    pub(super) struct BillingEntry {
+        id: i32,
+        #[schema(example = "Buy groceries")]
+        value: String,
+        done: bool,
+    }
+
+	#[derive(Serialize, Deserialize, ToSchema, Clone)]
+    pub(super) struct BillingResult {
+        status: String,
+    }
+
+	#[utoipa::path(
+        post,
+        path = "/v1/billing",
+        responses(
+            (status = 200, description = "List all todos successfully", body = [BillingResult])
+        )
+    )]
+	pub(super) async fn add_entries() -> (StatusCode, &'static str) {
+		let r ="OK";
+		(StatusCode::CREATED, r)
+	}
 }

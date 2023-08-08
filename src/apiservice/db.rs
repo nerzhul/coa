787d6b5b53fab89e233cc69536d07bc66c864fbf
Loic Blot <loic.blot@unix-experience.fr>
2023-08-08 10:18:12 +0200

    prepare some pricing/invoice queries

--- src/apiservice/db.rs
+++ src/apiservice/db.rs
@@ -2,6 +2,11 @@ use deadpool_postgres::{Manager, ManagerConfig, Pool, RecyclingMethod};
 use tokio_postgres::NoTls;
 use std::option::Option;
 
+const STMT_GET_PRICING: &str = "SELECT price, period, description FROM pricing WHERE id = $1";
+const STMT_GET_PRICING_ID_BY_OBJECT_TYPE: &str = "SELECT id FROM pricing WHERE object_type = $1";
+const STMT_ADD_INVOICE: &str = "INSERT INTO invoice(object_type, object_name, start_time, end_time, price_id VALUES ($1, $2, $3, $4, $5)";
+const STMT_GET_INVOICE_ID_BY_END_TIME: &str = "SELECT id FROM invoice WHERE object_type = $1 AND object_name = $2 AND end_time >= $3";
+
 pub async fn create_pool(db_host: Option<String>, db_name: Option<String>, username: Option<String>, password: Option<String>, pool_size: usize) -> deadpool_postgres::Pool {
 	let mut cfg = tokio_postgres::Config::new();
 	match db_host {
